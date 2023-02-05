b9e2e4690bd90b8ff246104dd0c086a50040e910
narukage <shunya0327@icloud.com>
2023-02-05 17:41:32 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+      // Your web app's Firebase configuration
+      const firebaseConfig = {
+        apiKey: "AIzaSyDlARAkQd8ag-mgZh60iihnaXlZiLordfM",
+        authDomain: "my-address-pj-858ea.firebaseapp.com",
+        projectId: "my-address-pj-858ea",
+        storageBucket: "my-address-pj-858ea.appspot.com",
+        messagingSenderId: "160104862580",
+        appId: "1:160104862580:web:ee43adaabb7bd5d116d221"
+      };
+    
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddressList from '../views/AddressList.vue'
+import AddressForm from '../views/AddressForm.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/address_list',
+    name: 'address_list',
+    component: AddressList
+  },
+  {
+    path: '/addresses/:address_id?edit',
+    name: 'address_edit',
+    component: AddressForm
   }
 ]
 
